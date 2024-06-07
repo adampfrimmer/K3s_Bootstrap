7349bd3fdeb038341052bc7b3e33ba9e2bbc4f6d
Adam Pfrimmer <pfrimmeradam@gmail.com>
2024-06-07 01:38:45 +0000

    try this instead

--- install_K3s.sh
+++ install_K3s.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --disable traefik,servicelb --write-kubeconfig-mode=644" sh -
 
-source <(kubectl completion bash) # set up autocomplete in bash into the current shell, bash-completion package should be installed first.
-echo "source <(kubectl completion bash)" >> ~/.bashrc # add autocomplete permanently to your bash shell.
+# Set up kubectl completion and aliases
+kubectl completion bash >> ~/.bashrc
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+echo "alias k=kubectl" >> ~/.bashrc
+echo "complete -o default -F __start_kubectl k" >> ~/.bashrc
 
-echo "alias k=kubectl" >> ~/.bashrc # add kubectl alias permanently to your bash shell.
-
-echo "complete -o default -F __start_kubectl k" >> ~/.bashrc # add enable completion to work with alias permanently to your bash shell.
+# Source the updated .bashrc to apply changes in the current shell
+source ~/.bashrc
