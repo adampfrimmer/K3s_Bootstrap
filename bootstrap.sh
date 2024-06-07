6c72db062828d281c5e7d9a660891c5fb666ab51
Adam Pfrimmer <pfrimmeradam@gmail.com>
2024-06-07 23:11:07 +0000

    test

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Ensure the script exits if any command fails
+set -e
+
+# Define variables for the repository owner, name, and branch
+owner="your_username"
+repo="your_repository"
+branch="dev"
+
+# Clone the repository
+git clone -b "$branch"  "https://github.com/adampfrimmer/K3s_Bootstrap.git"
+
+# Change directory to the cloned repository
+cd K3s_Bootstrap
+
+# Run the script that needs to know the branch
+./helper.sh
+cd ..
