1f8a830b40a310dbee76ed6b0e4290704f95dd47
Adam Pfrimmer <pfrimmeradam@gmail.com>
2024-06-07 02:08:17 +0000

    test

--- install_K3s.sh
+++ install_K3s.sh
@@ -11,7 +11,7 @@ cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
 echo "Installing helm"
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
+chmod 700 ./get_helm.sh
 ./get_helm.sh
 rm ./get_helm.sh
 
