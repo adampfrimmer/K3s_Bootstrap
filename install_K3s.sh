325f766cd87ee09a95887df6245e44160c6dcda3
Adam Pfrimmer <pfrimmeradam@gmail.com>
2024-06-07 02:02:23 +0000

    add helm install

--- install_K3s.sh
+++ install_K3s.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 # Install k3s
+echo "Installing K3s from https://get.k3s.io with traefik and servicelb disabled"
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --disable traefik,servicelb --write-kubeconfig-mode=644" sh -
+
+echo "Copying /etc/rancher/k3s/k3s.yaml to ~/.kube/config"
+mkdir -p ~/.kube
+cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+
+echo "Installing helm"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sh
 
 # Set up kubectl completion and aliases
 kubectl completion bash >> ~/.bashrc
@@ -9,6 +17,7 @@ echo "source <(kubectl completion bash)" >> ~/.bashrc
 echo "alias k=kubectl" >> ~/.bashrc
 echo "complete -o default -F __start_kubectl k" >> ~/.bashrc
 
-echo "Hello"
 # Source the updated .bashrc to apply changes in the current shell
-echo "Run the following command to apply the changes: source ~/.bashrc"
+echo "Run the following command to apply the changes to .bashrc: source ~/.bashrc"
+
+
