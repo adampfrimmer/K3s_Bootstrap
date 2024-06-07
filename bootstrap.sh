ee57fd8333cf1260c69bfd7371fb7c567178d7f8
Adam Pfrimmer <pfrimmeradam@gmail.com>
2024-06-07 23:32:04 +0000

    test

--- bootstrap.sh
+++ bootstrap.sh
@@ -47,31 +47,3 @@ fi
 
 echo "Hello 1"
 ls -al
-
-
-
-
-
-
-
-# # # # Define variables for the repository owner, name, and branch
-# # # owner="your_username"
-# # # repo="your_repository"
-# # # branch="dev"
-
-# # # # Clone the repository
-# # # git clone -b "$branch"  "https://github.com/adampfrimmer/K3s_Bootstrap.git"
-
-# # # echo "hello"
-# # # # Change directory to the cloned repository
-# # # cd K3s_Bootstrap
-# # # pwd
-# # # # Run the script that needs to know the branch
-# # # sudo chmod +x helper.sh
-# # # ./helper.sh
-
-# # # cd ..
-
-# # # sudo rm -r K3s_Bootstrap/
-
-
