feaef2bd62942fd9247647a9cb832d8116b456fe
Adam Pfrimmer <pfrimmeradam@gmail.com>
2024-06-08 01:04:11 +0000

    change it up

--- install_K3s.sh
+++ install_K3s.sh
@@ -27,3 +27,5 @@ echo "complete -o default -F __start_kubectl k" >> ~/.bashrc
 # Source the updated .bashrc to apply changes in the current shell
 echo "Run the following command to apply the changes to .bashrc: source ~/.bashrc"
 
+./helper.sh
+
